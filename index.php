f3ab694df455fb762603f1e3469f26157254c2d5
Gravity <gravityworkflowpro@gmail.com>
2025-01-09 17:14:59 -0500

    Gravity Theme

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Main template file.
+ *
+ * @package Gravity Theme
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="description" content="Gravity Theme" />
+        <meta name="keywords" content="Gravity Theme for WordPress" />
+        <meta name="author" content="Yulia Savinkova" />
+        <title>Gravity Theme</title>
+    </head>
+    <body>Hello Gravity</body>
+</html>
